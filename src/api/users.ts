8251f8e2c0013c8f9a5cceb398a40774af6075e9
Trésor Ndayikeza <160187183+kiddocoder@users.noreply.github.com>
2025-03-27 04:42:26 +0200

    Update users.ts

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,11 +1,11 @@
 import { API } from "../config/axios";
 
 export const signUpUser = async (data:any)=>{
-    const  res = await API.post("api/users", data);
+    const  res = await API.post("users", data);
     return res.data;
 }
 
 export const loginUser = async (data:any) =>{
-    const res = await API.post("api/users/login", data);
+    const res = await API.post("users/login", data);
     return res.data;
 }
